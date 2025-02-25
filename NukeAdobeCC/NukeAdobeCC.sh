2486d5b40297a63e9f0c394954821a3e11cbbea7
Scott Kendall <kendals@mac.com>
2025-02-25 08:08:51 -0500

    Perform a 'Seek & Destroy' on any Adobe applications on the Mac

---
+++ NukeAdobeCC/NukeAdobeCC.sh
@@ -0,0 +1,239 @@
+#!/bin/zsh
+#
+# NukeAdbobeCC
+#
+# by: Scott Kendall
+#
+# Written:  09/06/2024
+# Last updated: 02/25/2025
+#
+# Script Purpose: Completely remove Adobe Creative Cloud Suite from a users mac
+#
+# 1.0 - Initial
+# 1.1 - Major code cleanup & documentation / Structred code to be more inline / consistent across all apps
+
+######################################################################################################
+#
+# Gobal "Common" variables
+#
+######################################################################################################
+
+LOGGED_IN_USER=$( scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
+USER_DIR=$( dscl . -read /Users/${LOGGED_IN_USER} NFSHomeDirectory | awk '{ print $2 }' )
+
+OS_PLATFORM=$(/usr/bin/uname -p)
+
+[[ "$OS_PLATFORM" == 'i386' ]] && HWtype="SPHardwareDataType.0.cpu_type" || HWtype="SPHardwareDataType.0.chip_type"
+
+SYSTEM_PROFILER_BLOB=$( /usr/sbin/system_profiler -json 'SPHardwareDataType')
+MAC_SERIAL_NUMBER=$( echo $SYSTEM_PROFILER_BLOB | /usr/bin/plutil -extract 'SPHardwareDataType.0.serial_number' 'raw' -)
+MAC_CPU=$( echo $SYSTEM_PROFILER_BLOB | /usr/bin/plutil -extract "${HWtype}" 'raw' -)
+MAC_HADWARE_CLASS=$( echo $SYSTEM_PROFILER_BLOB | /usr/bin/plutil -extract 'SPHardwareDataType.0.machine_name' 'raw' -)
+MAC_RAM=$( echo $SYSTEM_PROFILER_BLOB | /usr/bin/plutil -extract 'SPHardwareDataType.0.physical_memory' 'raw' -)
+FREE_DISK_SPACE=$(($( /usr/sbin/diskutil info / | /usr/bin/grep "Free Space" | /usr/bin/awk '{print $6}' | /usr/bin/cut -c 2- ) / 1024 / 1024 / 1024 ))
+MACOS_VERSION=$( sw_vers -productVersion | xargs)
+
+SUPPORT_DIR="/Library/Application Support/GiantEagle"
+SD_BANNER_IMAGE="${SUPPORT_DIR}/SupportFiles/GE_SD_BannerImage.png"
+LOG_STAMP=$(echo $(/bin/date +%Y%m%d))
+LOG_DIR="${SUPPORT_DIR}/logs"
+
+ICON_FILES="/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/"
+
+# Swift Dialog version requirements
+
+SW_DIALOG="/usr/local/bin/dialog"
+[[ -e "${SW_DIALOG}" ]] && SD_VERSION=$( ${SW_DIALOG} --version) || SD_VERSION="0.0.0"
+MIN_SD_REQUIRED_VERSION="2.3.3"
+DIALOG_INSTALL_POLICY="install_SwiftDialog"
+SUPPORT_FILE_INSTALL_POLICY="install_SymFiles"
+
+###################################################
+#
+# App Specfic variables (Feel free to change these)
+#
+###################################################
+
+BANNER_TEXT_PADDING="      " #5 spaces to accomodate for icon offset
+SD_WINDOW_TITLE="${BANNER_TEXT_PADDING}Remove Adobe Suite"
+SD_INFO_BOX_MSG=""
+LOG_FILE="${LOG_DIR}/RemoveAdobe.log"
+SD_ICON_FILE="/Applications/Utilities/Adobe Creative Cloud/ACC/Creative Cloud.app"
+
+SD_DIALOG_GREETING=$((){print Good ${argv[2+($1>11)+($1>18)]}} ${(%):-%D{%H}} morning afternoon evening)
+
+##################################################
+#
+# Passed in variables
+# 
+#################################################
+
+JAMF_LOGGED_IN_USER=$3                          # Passed in by JAMF automatically
+SD_FIRST_NAME="${(C)JAMF_LOGGED_IN_USER%%.*}"
+
+####################################################################################################
+#
+# Functions
+#
+####################################################################################################
+
+function create_log_directory ()
+{
+    # Ensure that the log directory and the log files exist. If they
+    # do not then create them and set the permissions.
+    #
+    # RETURN: None
+
+	# If the log directory doesnt exist - create it and set the permissions
+	[[ ! -d "${LOG_DIR}" ]] && /bin/mkdir -p "${LOG_DIR}"
+	/bin/chmod 755 "${LOG_DIR}"
+
+	# If the log file does not exist - create it and set the permissions
+	[[ ! -f "${LOG_FILE}" ]] && /usr/bin/touch "${LOG_FILE}"
+	/bin/chmod 644 "${LOG_FILE}"
+}
+
+function logMe () 
+{
+    # Basic two pronged logging function that will log like this:
+    #
+    # 20231204 12:00:00: Some message here
+    #
+    # This function logs both to STDOUT/STDERR and a file
+    # The log file is set by the $LOG_FILE variable.
+    #
+    # RETURN: None
+    echo "${1}" 1>&2
+    echo "$(/bin/date '+%Y-%m-%d %H:%M:%S'): ${1}" | tee -a "${LOG_FILE}"
+}
+
+function check_swift_dialog_install ()
+{
+    # Check to make sure that Swift Dialog is installed and functioning correctly
+    # Will install process if missing or corrupted
+    #
+    # RETURN: None
+
+    logMe "Ensuring that swiftDialog version is installed..."
+    if [[ ! -x "${SW_DIALOG}" ]]; then
+        logMe "Swift Dialog is missing or corrupted - Installing from JAMF"
+        install_swift_dialog
+        SD_VERSION=$( ${SW_DIALOG} --version)        
+    fi
+
+    if ! is-at-least "${MIN_SD_REQUIRED_VERSION}" "${SD_VERSION}"; then
+        logMe "Swift Dialog is outdated - Installing version '${MIN_SD_REQUIRED_VERSION}' from JAMF..."
+        install_swift_dialog
+    else    
+        logMe "Swift Dialog is currently running: ${SD_VERSION}"
+    fi
+}
+
+function install_swift_dialog ()
+{
+    # Install Swift dialog From JAMF
+    # PARMS Expected: DIALOG_INSTALL_POLICY - policy trigger from JAMF
+    #
+    # RETURN: None
+
+	/usr/local/bin/jamf policy -trigger ${DIALOG_INSTALL_POLICY}
+}
+
+function check_support_files ()
+{
+    [[ ! -e "${SD_BANNER_IMAGE}" ]] && /usr/local/bin/jamf policy -trigger ${SUPPORT_FILE_INSTALL_POLICY}
+}
+
+function create_infobox_message()
+{
+	################################
+	#
+	# Swift Dialog InfoBox message construct
+	#
+	################################
+
+	SD_INFO_BOX_MSG="## System Info ##\n"
+	SD_INFO_BOX_MSG+="${MAC_CPU}<br>"
+	SD_INFO_BOX_MSG+="${MAC_SERIAL_NUMBER}<br>"
+	SD_INFO_BOX_MSG+="${MAC_RAM} RAM<br>"
+	SD_INFO_BOX_MSG+="${FREE_DISK_SPACE}GB Available<br>"
+	SD_INFO_BOX_MSG+="macOS ${MACOS_VERSION}<br>"
+}
+
+function cleanup_and_exit ()
+{
+	[[ -f ${JSON_OPTIONS} ]] && /bin/rm -rf ${JSON_OPTIONS}
+	[[ -f ${TMP_FILE_STORAGE} ]] && /bin/rm -rf ${TMP_FILE_STORAGE}
+    [[ -f ${DIALOG_COMMAND_FILE} ]] && /bin/rm -rf ${DIALOG_COMMAND_FILE}
+	exit 0
+}
+
+function delete_files ()
+{
+    rm -rf /Applications/Adobe*
+    rm -rf /Applications/Utilities/Adobe*
+    rm -rf /Library/Application\ Support/Adobe
+    rm -rf /Library/Preferences/com.adobe.*
+    rm -rf /Library/PrivilegedHelperTools/com.adobe.*
+    rm -rf /private/var/db/receipts/com.adobe.*
+    rm -rf ${USER_DIR}/Library/Application\ Support/Adobe*
+    rm -rf ${USER_DIR}/Library/Application\ Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.adobe*
+    rm -rf ${USER_DIR}/Library/Application\ Support/CrashReporter/Adobe*
+    rm -rf ${USER_DIR}/Library/Caches/Adobe
+    rm -rf ${USER_DIR}/Library/Caches/com.Adobe.*
+    rm -rf ${USER_DIR}/Library/Caches/com.adobe.*
+    rm -rf ${USER_DIR}/Library/Cookies/com.adobe.*
+    rm -rf ${USER_DIR}/Library/Logs/Adobe*
+    rm -rf ${USER_DIR}/Library/PhotoshopCrashes
+    rm -rf ${USER_DIR}/Library/Preferences/Adobe*
+    rm -rf ${USER_DIR}/Library/Preferences/com.adobe.*
+    rm -rf ${USER_DIR}/Library/Preferences/Macromedia*
+    rm -rf ${USER_DIR}/Library/Saved\ Application\ State/com.adobe.*
+    logMe "Delete Applications & Support Files"
+}
+
+function welcomemsg ()
+{
+    messagebody="This script is designed to completely remove the all of Adobe applications from your system, in case you are having issues launching any of the products.\n\n"
+    messagebody+="You will need to reinstall the Adobe Creative Cloud application from Self Service."
+
+	MainDialogBody=(
+        --message "${messagebody}"
+		--icon "${SD_ICON_FILE}"
+        --overlayicon "SF=trash.fill,color=black,weight=light"
+		--height 500
+        --width 750
+		--ontop
+        --infobox "${SD_INFO_BOX_MSG}"
+		--bannerimage "${SD_BANNER_IMAGE}"
+		--bannertitle "${SD_WINDOW_TITLE}"
+        --titlefont shadow=1
+		--button1text "Delete"
+		--button2text "Cancel"
+		--buttonstyle center
+    )
+
+	# Show the dialog screen and allow the user to choose
+
+	"${SW_DIALOG}" "${MainDialogBody[@]}" 2>/dev/null
+    returnCode=$?
+
+	# User wants to continue, so delete the files
+
+	[[ ${returnCode} -eq 0 ]] && delete_files
+
+}
+
+############################
+#
+# Start of Main Script
+#
+#############################
+
+autoload 'is-at-least'
+check_swift_dialog_install
+check_support_files
+create_infobox_message
+create_log_directory
+welcomemsg
+cleanup_and_exit
---
+++ NukeAdobeCC/README.md
@@ -0,0 +1,9 @@
+## Nuke Adobe CC
+
+Sometimes applications in the Adobe CC suite just refuse to work properly and the "offical" Adobe uninstaller just doesn't seem to fix the issue, so here is a script to do a "seek & destroy" of anything Adobe related on the user's Mac.
+
+![](/NukeAdobeCC/NukeAdobeCC.png)
+
+##### _v1.0 - Initial Commit_
+##### _v1.1 - Major code cleanup & documentation / Structred code to be more inline / consistent across all apps_
+
