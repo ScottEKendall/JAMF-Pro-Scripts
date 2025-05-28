6b37ffc0d48d9f518e5cd1b4c108a2171e7c4919
Scott Kendall <kendals@mac.com>
2025-05-28 07:24:39 -0400

    Remove the MAC_HADWARE_CLASS item as it was misspelled and not used anymore

--- PasswordExpire/PasswordExpire.sh
+++ PasswordExpire/PasswordExpire.sh
@@ -5,9 +5,10 @@
 # Purpose: Provide user notifications of a password expiration.
 #
 # Created Date: 04/18/2025
-# Last modified: 04/18/2025
+# Last modified: 05/28/2025
 #
 # v1.0 - Inital script
+# v1.1 - Remove the MAC_HADWARE_CLASS item as it was misspelled and not used anymore...
 #
 # Expected Paramaters: 
 # #4 - Title
@@ -33,7 +34,6 @@ OS_PLATFORM=$(/usr/bin/uname -p)
 SYSTEM_PROFILER_BLOB=$( /usr/sbin/system_profiler -json 'SPHardwareDataType')
 MAC_SERIAL_NUMBER=$( echo $SYSTEM_PROFILER_BLOB | /usr/bin/plutil -extract 'SPHardwareDataType.0.serial_number' 'raw' -)
 MAC_CPU=$( echo $SYSTEM_PROFILER_BLOB | /usr/bin/plutil -extract "${HWtype}" 'raw' -)
-MAC_HADWARE_CLASS=$( echo $SYSTEM_PROFILER_BLOB | /usr/bin/plutil -extract 'SPHardwareDataType.0.machine_name' 'raw' -)
 MAC_RAM=$( echo $SYSTEM_PROFILER_BLOB | /usr/bin/plutil -extract 'SPHardwareDataType.0.physical_memory' 'raw' -)
 FREE_DISK_SPACE=$(($( /usr/sbin/diskutil info / | /usr/bin/grep "Free Space" | /usr/bin/awk '{print $6}' | /usr/bin/cut -c 2- ) / 1024 / 1024 / 1024 ))
 MACOS_VERSION=$( sw_vers -productVersion | xargs)
@@ -70,9 +70,8 @@ SD_DIALOG_GREETING=$((){print Good ${argv[2+($1>11)+($1>18)]}} ${(%):-%D{%H}} mo
 JSS_FILE="/Library/Managed Preferences/com.gianteagle.jss.plist"
 SD_IMAGE_TO_DISPLAY="/Library/Application Support/GiantEagle/SupportFiles/PasswordChange.png"
 SD_IMAGE_POLICY="install_passwordSS"
-SD_ICON_PRIMARY="AlertNoteIcon.icns"
 SD_TIMER="240"
-SD_ICON_PRIMARY="${ICON_FILES}${SD_ICON_PRIMARY}"
+SD_ICON_PRIMARY="${ICON_FILES}AlertNoteIcon.icns"
 
 ##################################################
 #
@@ -83,7 +82,6 @@ SD_ICON_PRIMARY="${ICON_FILES}${SD_ICON_PRIMARY}"
 JAMF_LOGGED_IN_USER=$3                          # Passed in by JAMF automatically
 SD_FIRST_NAME="${(C)JAMF_LOGGED_IN_USER%%.*}"
 PASSWORD_EXPIRE_IN_DAYS=$4
-
 
 ####################################################################################################
 #
@@ -117,7 +115,6 @@ function logMe ()
     # The log file is set by the $LOG_FILE variable.
     #
     # RETURN: None
-    echo "${1}" 1>&2
     echo "$(/bin/date '+%Y-%m-%d %H:%M:%S'): ${1}" | tee -a "${LOG_FILE}"
 }
 
