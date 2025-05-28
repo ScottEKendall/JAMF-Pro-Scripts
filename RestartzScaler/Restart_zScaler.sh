008231dd0bddef08d8966e935baa5134e4da2ed7
Scott Kendall <kendals@mac.com>
2025-05-28 07:25:28 -0400

    Remove the MAC_HADWARE_CLASS item as it was misspelled and not used anymore

--- RestartzScaler/Restart_zScaler.sh
+++ RestartzScaler/Restart_zScaler.sh
@@ -5,11 +5,12 @@
 # by: Scott Kendall
 #
 # Written: 03/12/2025
-# Last updated: 03/12/2025
+# Last updated: 05/28/2025
 #
 # Script Purpose: Attempt to restart the zScaler service and prompt user if failure
 #
 # 1.0 - Initial
+# 1.1 - Remove the MAC_HADWARE_CLASS item as it was misspelled and not used anymore...
 
 ######################################################################################################
 #
@@ -27,7 +28,6 @@ OS_PLATFORM=$(/usr/bin/uname -p)
 SYSTEM_PROFILER_BLOB=$( /usr/sbin/system_profiler -json 'SPHardwareDataType')
 MAC_SERIAL_NUMBER=$( echo $SYSTEM_PROFILER_BLOB | /usr/bin/plutil -extract 'SPHardwareDataType.0.serial_number' 'raw' -)
 MAC_CPU=$( echo $SYSTEM_PROFILER_BLOB | /usr/bin/plutil -extract "${HWtype}" 'raw' -)
-MAC_HADWARE_CLASS=$( echo $SYSTEM_PROFILER_BLOB | /usr/bin/plutil -extract 'SPHardwareDataType.0.machine_name' 'raw' -)
 MAC_RAM=$( echo $SYSTEM_PROFILER_BLOB | /usr/bin/plutil -extract 'SPHardwareDataType.0.physical_memory' 'raw' -)
 FREE_DISK_SPACE=$(($( /usr/sbin/diskutil info / | /usr/bin/grep "Free Space" | /usr/bin/awk '{print $6}' | /usr/bin/cut -c 2- ) / 1024 / 1024 / 1024 ))
 MACOS_VERSION=$( sw_vers -productVersion | xargs)
@@ -105,7 +105,6 @@ function logMe ()
     # The log file is set by the $LOG_FILE variable.
     #
     # RETURN: None
-    echo "${1}" 1>&2
     echo "$(/bin/date '+%Y-%m-%d %H:%M:%S'): ${1}" | tee -a "${LOG_FILE}"
 }
 
