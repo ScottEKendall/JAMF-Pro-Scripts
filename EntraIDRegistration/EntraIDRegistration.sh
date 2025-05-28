56617f0c6577b3b6e14829c49bd6f7fddee22414
Scott Kendall <kendals@mac.com>
2025-05-28 07:22:48 -0400

    Remove the MAC_HADWARE_CLASS item as it was misspelled and not used anymore

--- EntraIDRegistration/EntraIDRegistration.sh
+++ EntraIDRegistration/EntraIDRegistration.sh
@@ -5,7 +5,7 @@
 # by: Scott Kendall
 #
 # Written: 02/03/2025
-# Last updated: 02/22/2025
+# Last updated: 05/28/2025
 #
 # Script Purpose: Backup the keychain file and delete the current keychain file(s)
 #
@@ -13,6 +13,7 @@
 # 1.1 - Code cleanup to be more consistant with all apps
 # 1.2 - Fixed issue of Register button not running the JAMF policy
 # 1.3 - Removed debug code and fix incorrect message on failure dialog
+# 1.4 - Remove the MAC_HADWARE_CLASS item as it was misspelled and not used anymore...
 #
 ######################################################################################################
 #
@@ -30,7 +31,6 @@ OS_PLATFORM=$(/usr/bin/uname -p)
 SYSTEM_PROFILER_BLOB=$( /usr/sbin/system_profiler -json 'SPHardwareDataType')
 MAC_SERIAL_NUMBER=$( echo $SYSTEM_PROFILER_BLOB | /usr/bin/plutil -extract 'SPHardwareDataType.0.serial_number' 'raw' -)
 MAC_CPU=$( echo $SYSTEM_PROFILER_BLOB | /usr/bin/plutil -extract "${HWtype}" 'raw' -)
-MAC_HADWARE_CLASS=$( echo $SYSTEM_PROFILER_BLOB | /usr/bin/plutil -extract 'SPHardwareDataType.0.machine_name' 'raw' -)
 MAC_RAM=$( echo $SYSTEM_PROFILER_BLOB | /usr/bin/plutil -extract 'SPHardwareDataType.0.physical_memory' 'raw' -)
 FREE_DISK_SPACE=$(($( /usr/sbin/diskutil info / | /usr/bin/grep "Free Space" | /usr/bin/awk '{print $6}' | /usr/bin/cut -c 2- ) / 1024 / 1024 / 1024 ))
 MACOS_VERSION=$( sw_vers -productVersion | xargs)
