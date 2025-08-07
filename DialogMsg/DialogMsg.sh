c9e0606058f80281e1eb4daa0e37783c80ae9bb7
Scott Kendall <kendals@mac.com>
2025-08-07 08:58:14 -0400

    Display a SwiftDialog formatted window to all JAMF users.  Now with dual language support

--- DialogMsg/DialogMsg.sh
+++ DialogMsg/DialogMsg.sh
@@ -5,7 +5,7 @@
 # Written by: Scott Kendall
 #
 # Created Date: 01/227/2025
-# Last modified: 06/23/2025
+# Last modified: 07/09/2025
 #
 # Script Purpose: Display a generic SWifDialog notification to JAMF users.  Pass in variables to customize display
 #
@@ -15,15 +15,17 @@
 #      - Added -ignorednd to make sure that the message is displayed regardless of focus setting
 #      - Will display the infobox items if you can the function first
 #      - Minimum version of SwiftDialog is now 2.5.0
+# v1.3 - Changed variable declarations around for better readability
 #
 # Expected Paramaters: 
 # #4 - Title
 # #5 - Full formatted message to display
-# #6 - Button1 Text
-# #7 - Image to display
-# #8 - JAMF policy to load image if it doeesn't exist
-# #9 - Notification icon name
-# #10 - Timer (in seconds) to wait until dismissal
+# #6 - Alternate language to display (formatted as <2 Digit Lang code> | <message>)
+# #7 - Button1 Text
+# #8 - Image to display
+# #9 - JAMF policy to load image if it doeesn't exist
+# #10 - Notification icon name
+# #11 - Timer (in seconds) to wait until dismissal
 
 ######################################################################################################
 #
@@ -34,38 +36,47 @@ export PATH=/usr/bin:/bin:/usr/sbin:/sbin
 LOGGED_IN_USER=$( scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
 USER_DIR=$( dscl . -read /Users/${LOGGED_IN_USER} NFSHomeDirectory | awk '{ print $2 }' )
 
-OS_PLATFORM=$(/usr/bin/uname -p)
-
-[[ "$OS_PLATFORM" == 'i386' ]] && HWtype="SPHardwareDataType.0.cpu_type" || HWtype="SPHardwareDataType.0.chip_type"
+[[ "$(/usr/bin/uname -p)" == 'i386' ]] && HWtype="SPHardwareDataType.0.cpu_type" || HWtype="SPHardwareDataType.0.chip_type"
 
 SYSTEM_PROFILER_BLOB=$( /usr/sbin/system_profiler -json 'SPHardwareDataType')
 MAC_CPU=$( echo $SYSTEM_PROFILER_BLOB | /usr/bin/plutil -extract "${HWtype}" 'raw' -)
 MAC_RAM=$( echo $SYSTEM_PROFILER_BLOB | /usr/bin/plutil -extract 'SPHardwareDataType.0.physical_memory' 'raw' -)
 FREE_DISK_SPACE=$(($( /usr/sbin/diskutil info / | /usr/bin/grep "Free Space" | /usr/bin/awk '{print $6}' | /usr/bin/cut -c 2- ) / 1024 / 1024 / 1024 ))
 
-SUPPORT_DIR="/Library/Application Support/GiantEagle"
-OVERLAY_ICON="${SUPPORT_DIR}/SupportFiles/DiskSpace.png"
-SD_BANNER_IMAGE="${SUPPORT_DIR}/SupportFiles/GE_SD_BannerImage.png"
-
-LOG_DIR="${SUPPORT_DIR}/logs"
-LOG_FILE="${LOG_DIR}/DialogNotify.log"
-LOG_STAMP=$(echo $(/bin/date +%Y%m%d))
+ICON_FILES="/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/"
 
 # Swift Dialog version requirements
 
 SW_DIALOG="/usr/local/bin/dialog"
-[[ -e "${SW_DIALOG}" ]] && SD_VERSION=$( ${SW_DIALOG} --version) || SD_VERSION="0.0.0"
 MIN_SD_REQUIRED_VERSION="2.5.0"
-DIALOG_INSTALL_POLICY="install_SwiftDialog"
-SUPPORT_FILE_INSTALL_POLICY="install_SymFiles"
+[[ -e "${SW_DIALOG}" ]] && SD_VERSION=$( ${SW_DIALOG} --version) || SD_VERSION="0.0.0"
 
-ICON_FILES="/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/"
-
-JSONOptions=$(mktemp /var/tmp/DialogNotify.XXXXX)
-BANNER_TEXT_PADDING="      "
-SD_INFO_BOX_MSG=""
 SD_DIALOG_GREETING=$((){print Good ${argv[2+($1>11)+($1>18)]}} ${(%):-%D{%H}} morning afternoon evening)
-SD_DEFAULT_LANGUAGE="EN"
+
+###################################################
+#
+# App Specfic variables (Feel free to change these)
+#
+###################################################
+
+# Support / Log files location
+
+SUPPORT_DIR="/Library/Application Support/GiantEagle"
+LOG_FILE="${SUPPORT_DIR}/DialogNotify.log"
+
+# Display items (banner / icon)
+
+BANNER_TEXT_PADDING="      " #5 spaces to accomodate for icon offset
+SD_BANNER_IMAGE="${SUPPORT_DIR}/SupportFiles/GE_SD_BannerImage.png"
+SD_IMAGE_TO_DISPLAY="${SUPPORT_DIR}/SupportFiles/PasswordChange.png"
+SD_OVERLAY_ICON="computer"
+
+# Trigger installs for Images & icons
+
+SUPPORT_FILE_INSTALL_POLICY="install_SymFiles"
+DIALOG_INSTALL_POLICY="install_SwiftDialog"
+
+SD_DEFAULT_LANGUAGE="NL" # Change your default language here!
 DISPLAY_MESSAGE=""
 
 ##################################################
@@ -102,6 +113,7 @@ function create_log_directory ()
     # RETURN: None
 
 	# If the log directory doesnt exist - create it and set the permissions
+    LOG_DIR=${LOG_FILE%/*}
 	[[ ! -d "${LOG_DIR}" ]] && /bin/mkdir -p "${LOG_DIR}"
 	/bin/chmod 755 "${LOG_DIR}"
 
@@ -169,7 +181,7 @@ function display_msg ()
 		--ontop
 		--icon "${SD_ICON_PRIMARY}"
         --titlefont shadow=1
-		--overlayicon computer
+		--overlayicon "${SD_OVERLAY_ICON}"
 		--bannerimage "${SD_BANNER_IMAGE}"
 		--bannertitle "${SD_WINDOW_TITLE}"
         --infobox "${SD_INFO_BOX_MSG}"
