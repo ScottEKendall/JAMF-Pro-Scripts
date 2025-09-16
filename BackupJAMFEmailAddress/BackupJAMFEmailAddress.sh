6b9342fff09fe868370efa6d1e9d146e00eb678e
Scott Kendall <106453475+ScottEKendall@users.noreply.github.com>
2025-09-16 10:28:23 -0400

    GUI Method for exporting emails into VCF format

--- BackupJAMFEmailAddress/BackupJAMFEmailAddress.sh
+++ BackupJAMFEmailAddress/BackupJAMFEmailAddress.sh
@@ -5,11 +5,17 @@
 # by: Scott Kendall
 #
 # Written: 05/28/2025
-# Last updated: 06/03/2025
+# Last updated: 09/16/2025
 #
 # Script Purpose: This script will extract all of the email addresses from your JAMF server and store them in local folder in a VCF format.
 # 1.0 - Initial
 # 1.1 - Created a few new functions to reduce complexity / document function details / renamed all JAMF functions to start with JAMF......
+# 1.2 - Verified working agains JAMF API 11.20
+#       Added option to detect which SS/SS+ we are using a grab the appropriate icon
+#       Now works with JAMF Client/Secret or Username/password authentication
+#       Change variable declare section around for better readability
+#       Changed to using JSON blobs vs XML Blobs
+#       Bumped Swift Dialog to v2.5.0
 
 ######################################################################################################
 #
@@ -17,48 +23,26 @@
 #
 ######################################################################################################
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin
-
 LOGGED_IN_USER=$( scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
 USER_DIR=$( dscl . -read /Users/${LOGGED_IN_USER} NFSHomeDirectory | awk '{ print $2 }' )
 
-OS_PLATFORM=$(/usr/bin/uname -p)
-
-[[ "$OS_PLATFORM" == 'i386' ]] && HWtype="SPHardwareDataType.0.cpu_type" || HWtype="SPHardwareDataType.0.chip_type"
+[[ "$(/usr/bin/uname -p)" == 'i386' ]] && HWtype="SPHardwareDataType.0.cpu_type" || HWtype="SPHardwareDataType.0.chip_type"
 
 SYSTEM_PROFILER_BLOB=$( /usr/sbin/system_profiler -json 'SPHardwareDataType')
-MAC_SERIAL_NUMBER=$( echo $SYSTEM_PROFILER_BLOB | /usr/bin/plutil -extract 'SPHardwareDataType.0.serial_number' 'raw' -)
 MAC_CPU=$( echo $SYSTEM_PROFILER_BLOB | /usr/bin/plutil -extract "${HWtype}" 'raw' -)
 MAC_RAM=$( echo $SYSTEM_PROFILER_BLOB | /usr/bin/plutil -extract 'SPHardwareDataType.0.physical_memory' 'raw' -)
 FREE_DISK_SPACE=$(($( /usr/sbin/diskutil info / | /usr/bin/grep "Free Space" | /usr/bin/awk '{print $6}' | /usr/bin/cut -c 2- ) / 1024 / 1024 / 1024 ))
-MACOS_VERSION=$( sw_vers -productVersion | xargs)
-
-SUPPORT_DIR="/Library/Application Support/GiantEagle"
-SD_BANNER_IMAGE="${SUPPORT_DIR}/SupportFiles/GE_SD_BannerImage.png"
-LOG_STAMP=$(echo $(/bin/date +%Y%m%d))
-LOG_DIR="${SUPPORT_DIR}/logs"
 
 ICON_FILES="/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/"
 
 # Swift Dialog version requirements
 
 SW_DIALOG="/usr/local/bin/dialog"
+MIN_SD_REQUIRED_VERSION="2.5.0"
 [[ -e "${SW_DIALOG}" ]] && SD_VERSION=$( ${SW_DIALOG} --version) || SD_VERSION="0.0.0"
-MIN_SD_REQUIRED_VERSION="2.3.3"
-DIALOG_INSTALL_POLICY="install_SwiftDialog"
-SUPPORT_FILE_INSTALL_POLICY="install_SymFiles"
 
-###################################################
-#
-# App Specfic variables (Feel free to change these)
-#
-###################################################
+# Make some temp files for this app
 
-BANNER_TEXT_PADDING="      " #5 spaces to accomodate for icon offset
-SD_WINDOW_TITLE="${BANNER_TEXT_PADDING}JAMF Retrieve Email"
-SD_INFO_BOX_MSG=""
-LOG_FILE="${LOG_DIR}/JAMFRetrieveEmail.log"
-SD_ICON_FILE=$ICON_FILES"ToolbarCustomizeIcon.icns"
-OVERLAY_ICON="/Applications/Self Service.app"
 JSON_DIALOG_BLOB=$(mktemp /var/tmp/JAMFRetrieveEmail.XXXXX)
 DIALOG_CMD_FILE=$(mktemp /var/tmp/JAMFRetrieveEmail.XXXXX)
 /bin/chmod 666 $JSON_DIALOG_BLOB
@@ -66,16 +50,46 @@ DIALOG_CMD_FILE=$(mktemp /var/tmp/JAMFRetrieveEmail.XXXXX)
 
 SD_DIALOG_GREETING=$((){print Good ${argv[2+($1>11)+($1>18)]}} ${(%):-%D{%H}} morning afternoon evening)
 
+###################################################
+#
+# App Specfic variables (Feel free to change these)
+#
+###################################################
+
+# Support / Log files location
+
+SUPPORT_DIR="/Library/Application Support/GiantEagle"
+LOG_FILE="${SUPPORT_DIR}/logs/JAMFRetrieveEmail.log"
+
+# Display items (banner / icon)
+
+BANNER_TEXT_PADDING="      " #5 spaces to accomodate for icon offset
+SD_WINDOW_TITLE="${BANNER_TEXT_PADDING}JAMF Retrieve Email"
+SD_BANNER_IMAGE="${SUPPORT_DIR}/SupportFiles/GE_SD_BannerImage.png"
+OVERLAY_ICON=""
+SD_ICON_FILE=$ICON_FILES"ToolbarCustomizeIcon.icns"
+
+# Trigger installs for Images & icons
+
+SUPPORT_FILE_INSTALL_POLICY="install_SymFiles"
+DIALOG_INSTALL_POLICY="install_SwiftDialog"
+JQ_FILE_INSTALL_POLICY="install_jq"
+CURRENT_EPOCH=$(date +%s)
+
+# Make some temp files for this app
+
 ##################################################
 #
 # Passed in variables
 # 
 #################################################
 
-JAMF_LOGGED_IN_USER="$3"                          # Passed in by JAMF automatically
+JAMF_LOGGED_IN_USER=${3:-"$LOGGED_IN_USER"}    # Passed in by JAMF automatically
 SD_FIRST_NAME="${(C)JAMF_LOGGED_IN_USER%%.*}"   
 CLIENT_ID="$4"
 CLIENT_SECRET="$5"
+
+[[ ${#CLIENT_ID} -gt 30 ]] && JAMF_TOKEN="new" || JAMF_TOKEN="classic" #Determine with JAMF creentials we are using
 
 ####################################################################################################
 #
@@ -91,6 +105,7 @@ function create_log_directory ()
     # RETURN: None
 
 	# If the log directory doesnt exist - create it and set the permissions
+    LOG_DIR=${LOG_FILE%/*}
 	[[ ! -d "${LOG_DIR}" ]] && /bin/mkdir -p "${LOG_DIR}"
 	/bin/chmod 755 "${LOG_DIR}"
 
@@ -160,10 +175,10 @@ function create_infobox_message()
 
 	SD_INFO_BOX_MSG="## System Info ##<br>"
 	SD_INFO_BOX_MSG+="${MAC_CPU}<br>"
-	SD_INFO_BOX_MSG+="${MAC_SERIAL_NUMBER}<br>"
+	SD_INFO_BOX_MSG+="{serialnumber}<br>"
 	SD_INFO_BOX_MSG+="${MAC_RAM} RAM<br>"
 	SD_INFO_BOX_MSG+="${FREE_DISK_SPACE}GB Available<br>"
-	SD_INFO_BOX_MSG+="macOS ${MACOS_VERSION}<br>"
+	SD_INFO_BOX_MSG+="{osname} {osversion}<br>"
 }
 
 function cleanup_and_exit ()
@@ -329,25 +344,21 @@ function create_display_list ()
     construct_dialog_header_settings "The following VCF Cards are being created from the JAMF server" > "${JSON_DIALOG_BLOB}"
     create_listitem_message_body "" "" "" "" "first"
 
-    echo $UserList | xmllint --xpath '//name' - | while IFS= read -r line; do
-        # Remove the opening tag
-        line="${line/<name>/}"
-        # Remove the closing tag
-        line="${line/<\/name>/}"
+    echo $UserList | jq -r '.users[].name' | while IFS= read -r line; do
         create_listitem_message_body "$line" "" "pending" "Pending..."
     done
     create_listitem_message_body "" "" "" "" "last"
     update_display_list "Create"
 }
 
-function extract_xml_data ()
+function extract_json_data ()
 {
     # PURPOSE: extract an XML strng from the passed string
     # RETURN: parsed XML string
     # PARAMETERS: $1 - XML "blob"
     #             $2 - String to extract
     # EXPECTED: None
-    echo $(echo "$1" | tr -d "[:cntrl:]" | xmllint --xpath 'string(//'${2}')' - 2>/dev/null)
+    echo $1 | jq -r ".user.${2}"
 }
 
 function make_apfs_safe ()
@@ -477,7 +488,7 @@ function JAMF_invalidate_token ()
     fi    
 }
 
-function JAMF_retrieve_xml_data_summary ()
+function JAMF_retrieve_json_data ()
 {    
     # PURPOSE: Extract the summary of the JAMF conmand results
     # RETURN: XML contents of command
@@ -486,10 +497,10 @@ function JAMF_retrieve_xml_data_summary ()
     #   JAMF_COMMAND_SUMMARY - specific JAMF API call to execute
     #   api_token - base64 hex code of your bearer token
     #   jamppro_url - the URL of your JAMF server   
-    echo $(/usr/bin/curl -s --header "Authorization: Bearer ${api_token}" -H "Accept: application/xml" "${jamfpro_url}${1}" )
+    echo $(/usr/bin/curl -s --header "Authorization: Bearer ${api_token}" -H "Accept: application/json" "${jamfpro_url}${1}" )
 }
 
-function JAMF_retrieve_xml_data_details ()
+function JAMF_retrieve_json_data_details ()
 {    
     # PURPOSE: Extract the summary of the JAMF conmand results
     # RETURN: XML contents of command
@@ -497,7 +508,7 @@ function JAMF_retrieve_xml_data_details ()
     # EXPECTED: 
     #   api_token - base64 hex code of your bearer token
     #   jamppro_url - the URL of your JAMF server   
-    xmlBlob=$(/usr/bin/curl -s --header "Authorization: Bearer ${api_token}" -H "Accept: application/xml" "${jamfpro_url}${1}")
+    jsonBlob=$(/usr/bin/curl -s --header "Authorization: Bearer ${api_token}" -H "Accept: application/json" "${jamfpro_url}${1}")
 }
 
 function JAMF_get_inventory_record ()
@@ -507,7 +518,7 @@ function JAMF_get_inventory_record ()
     # PARMS: $1 - Section of inventory record to retrieve (GENERAL, DISK_ENCRYPTION, PURCHASING, APPLICATIONS, STORAGE, USER_AND_LOCATION, CONFIGURATION_PROFILES, PRINTERS, 
     #                                                      SERVICES, HARDWARE, LOCAL_USER_ACCOUNTS, CERTIFICATES, ATTACHMENTS, PLUGINS, PACKAGE_RECEIPTS, FONTS, SECURITY, OPERATING_SYSTEM,
     #                                                      LICENSED_SOFTWARE, IBEACONS, SOFTWARE_UPDATES, EXTENSION_ATTRIBUTES, CONTENT_CACHING, GROUP_MEMBERSHIPS)
-    retval=$(/usr/bin/curl --silent --fail  -H "Authorization: Bearer ${api_token}" -H "Accept: application/json" "${jamfpro_url}api/v1/computers-inventory/$1?section=$2" 2>/dev/null)
+    retval=$(/usr/bin/curl --silent --fail  -H "Authorization: Bearer ${api_token}" -H "Accept: application/json" "${jamfpro_url}api/v2/computers-inventory/$1?section=$2" 2>/dev/null)
     echo $retval | tr -d '\n'
 }
 
@@ -522,18 +533,17 @@ function extract_user_details ()
     # PURPOSE: extract the XNL string from the JAMF ID and create a list of found items
     # RETURN: None
     # PARAMETERS: $1 - API substrint to call from JAMF
-    # EXPECTED: xmlBlob should be globally defined
+    # EXPECTED: jsonBlob should be globally defined
     declare managed && managed=true
 
 	[[ -z "${1}" ]] && return 0
 
-    JAMF_retrieve_xml_data_details "JSSResource/users/id/$1"
-
-    userShortName=$(extract_xml_data $xmlBlob "name")
-    userFullName=$(extract_xml_data $xmlBlob  "full_name")
-    userEmail=$(extract_xml_data $xmlBlob "email_address")
-    userPosition=$(extract_xml_data $xmlBlob "position")
-
+    JAMF_retrieve_json_data_details "JSSResource/users/id/$1"
+    
+    userShortName=$(extract_json_data $jsonBlob "name")
+    userFullName=$(extract_json_data $jsonBlob  "full_name")
+    userEmail=$(extract_json_data $jsonBlob "email_address")
+    userPosition=$(extract_json_data $jsonBlob "position")
 
     # if the script shows as empty (probably due to import issues), then show as a failure and report it
     if [[ -z $userFullName ]]; then
@@ -544,7 +554,7 @@ function extract_user_details ()
 
         if [[ $ssOnlyManagedUsers == "true" ]]; then
             # if they choose to include non-managed users, then we will not check for managed systems
-            managed=$(extract_assigned_systems ${xmlBlob})
+            managed=$(extract_assigned_systems ${jsonBlob})
         fi
 
         # if they choose to show managed users only, then check if the user has any managed systems
@@ -580,8 +590,8 @@ function extract_assigned_systems ()
     # RETURN: None
     # PARAMETERS: $1 - XML string to parse
     # EXPECTED: None
-    declare managed && managed=false
-    computer_ids=($(echo "$1" | xmllint --xpath "//computer/id/text()" - )) #2>/dev/null)
+    declare managed=false
+    computer_ids=($(echo "$1" | jq -r ".user.links.computers[].id"))
  
     # Loop through and evaluate each computer ID
     for id in "${computer_ids[@]}"; do
@@ -604,7 +614,7 @@ declare jamfpro_url
 declare ssStoragePath
 declare ssOnlyManagedUsers
 declare UserList
-declare xmlBlob
+declare jsonBlob
 declare -a failedMigration
 declare userCount
 
@@ -616,11 +626,10 @@ welcomemsg
 
 JAMF_check_connection
 JAMF_get_server
-JAMF_get_classic_api_token
+[[ $JAMF_TOKEN == "new" ]] && JAMF_get_access_token || JAMF_get_classic_api_token
 
-UserList=$(JAMF_retrieve_xml_data_summary "JSSResource/users")
-UserIDs=$(echo $UserList | xmllint --xpath '//id' - 2>/dev/null)
-UserIDs=($(echo "$UserIDs" | grep -Eo "[0-9]+"))
+UserList=$(JAMF_retrieve_json_data "JSSResource/users")
+UserIDs=($(echo $UserList | jq -r '.users[].id'))
 userCount=${#UserIDs[@]}
 
 create_display_list
