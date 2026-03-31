5224460506be854d2a541c72021b18be3b171ea8
Scott Kendall <106453475+ScottEKendall@users.noreply.github.com>
2026-03-31 16:14:19 -0400

    Updated to support Dialog 3.10 features - includes banner subtitles, color & offset

--- GetBundleID/GetBundleID.sh
+++ GetBundleID/GetBundleID.sh
@@ -5,7 +5,7 @@
 # by: Scott Kendall
 #
 # Written: 07/11/2025
-# Last updated: 03/15x/2026
+# Last updated: 04/01/2026
 #
 # Script Purpose: Extract the bundle ID of all of the apps found in a given directory
 #
@@ -20,6 +20,8 @@
 #       Fixed variable names in the defaults file section
 # 2.0 - Now includes TeamID in the listing as well
 #       Changed the order of the items in the welcome screen
+# 2.1 - Updated SD Version requirements to 3.1.0
+#       Added ability to set subtitle, color, and padding from defaults file
 
 ######################################################################################################
 #
@@ -42,7 +44,7 @@ ICON_FILES="/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/"
 # Swift Dialog version requirements
 
 SW_DIALOG="/usr/local/bin/dialog"
-MIN_SD_REQUIRED_VERSION="2.5.0"
+MIN_SD_REQUIRED_VERSION="3.1.0"
 [[ -e "${SW_DIALOG}" ]] && SD_VERSION=$( ${SW_DIALOG} --version) || SD_VERSION="0.0.0"
 
 SD_DIALOG_GREETING=$((){print Good ${argv[2+($1>11)+($1>18)]}} ${(%):-%D{%H}} morning afternoon evening)
@@ -66,13 +68,17 @@ if [[ -f "$DEFAULTS_DIR" ]]; then
     echo "Found Defaults Files.  Reading in Info"
     SUPPORT_DIR=$(defaults read "$DEFAULTS_DIR" SupportFiles)
     SD_BANNER_IMAGE="${SUPPORT_DIR}$(defaults read "$DEFAULTS_DIR" BannerImage)"
-    SPACING=$(defaults read "$DEFAULTS_DIR" BannerPadding)
+    BANNER_TEXT_PADDING=$(defaults read "$DEFAULTS_DIR" BannerPadding)
+    BANNER_SUBTITLE=$(defaults read "$DEFAULTS_DIR" BannerSubtitle)
+    BANNER_TEXT_COLOR=$(defaults read "$DEFAULTS_DIR" TitleFontColor)
 else
+    echo "Defaults Files Not Found.  Creating with default values"
     SUPPORT_DIR="/Library/Application Support/GiantEagle"
     SD_BANNER_IMAGE="${SUPPORT_DIR}/SupportFiles/GE_SD_BannerImage.png"
-    SPACING=5 #5 spaces to accommodate for icon offset
+    BANNER_TEXT_PADDING=10 #10 spaces to accommodate for icon offset
+    BANNER_SUBTITLE=""
+    BANNER_TEXT_COLOR="white"
 fi
-BANNER_TEXT_PADDING="${(j::)${(l:$SPACING:: :)}}"
 
 # Log files location
 
@@ -80,7 +86,7 @@ LOG_FILE="${SUPPORT_DIR}/logs/${SCRIPT_NAME}.log"
 
 # Display items (banner / icon)
 
-SD_WINDOW_TITLE="${BANNER_TEXT_PADDING}Extract BundleID"
+SD_WINDOW_TITLE="Extract BundleID"
 SD_ICON_FILE=$ICON_FILES"ToolbarCustomizeIcon.icns"
 OVERLAY_ICON="/System/Applications/App Store.app"
 
@@ -203,12 +209,13 @@ function welcomemsg ()
 
 	MainDialogBody=(
         --message "$SD_DIALOG_GREETING $SD_FIRST_NAME. $message"
-        --titlefont shadow=1
+        --bannerimage "${SD_BANNER_IMAGE}"
+        --subtitle "${BANNER_SUBTITLE}"
+        --bannertitle "${SD_WINDOW_TITLE}"
+        --titlefont "shadow=1, color=${BANNER_TEXT_COLOR}, offset=${BANNER_TEXT_PADDING}"
         --ontop
         --icon "${SD_ICON_FILE}"
         --overlayicon "${OVERLAY_ICON}"
-        --bannerimage "${SD_BANNER_IMAGE}"
-        --bannertitle "${SD_WINDOW_TITLE}"
         --infobox "${SD_INFO_BOX_MSG}"
         --vieworder "textfield, checkbox"
         --textfield "Select a file location to scan",fileselect,filetype=folder,required,name=fileLocation
@@ -272,7 +279,8 @@ function construct_dialog_header_settings ()
         "overlayicon" : "'${OVERLAY_ICON}'",
         "ontop" : "true",
         "bannertitle" : "'${SD_WINDOW_TITLE}'",
-        "titlefont" : "shadow=1",
+        "subtitle" : "'${BANNER_SUBTITLE}'",
+        "titlefont" : "shadow=1, color='${BANNER_TEXT_COLOR}', offset='${BANNER_TEXT_PADDING}'",
         "button1text" : "OK",
         "moveable" : "true",
         "width" : "1100",
