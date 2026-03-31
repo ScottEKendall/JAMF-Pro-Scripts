fb78ddd1d85b3bc09bf442277155c1eac677bd09
Scott Kendall <106453475+ScottEKendall@users.noreply.github.com>
2026-03-31 14:56:58 -0400

    Updated to support Dialog 3.10 features - includes banner subtitles, color & offset

--- EntraIDRegistration/EntraIDRegistration.sh
+++ EntraIDRegistration/EntraIDRegistration.sh
@@ -5,7 +5,7 @@
 # by: Scott Kendall
 #
 # Written: 02/03/2025
-# Last updated: 03/13/2026
+# Last updated: 04/01/2026
 #
 # Script Purpose: Check if the user is registered for EntraID and display status.  If not registered, provide option to register via JAMF policy.
 #
@@ -24,7 +24,8 @@
 # 1.7 - Changed JAMF 'policy -trigger' to 'JAMF policy -event'
 #       Optimized "Common" section for better performance
 #       Fixed variable names in the defaults file section
-
+# 2.0 - Updated SD Version requirements to 3.1.0
+#       Added ability to set subtitle, color, and padding from defaults file
 #
 ######################################################################################################
 #
@@ -49,7 +50,7 @@ ICON_FILES="/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/"
 # Swift Dialog version requirements
 
 SW_DIALOG="/usr/local/bin/dialog"
-MIN_SD_REQUIRED_VERSION="2.5.0"
+MIN_SD_REQUIRED_VERSION="3.1.0"
 HOUR=$(date +%H)
 case $HOUR in
     0[0-9]|1[0-1]) GREET="morning" ;;
@@ -70,13 +71,17 @@ if [[ -f "$DEFAULTS_DIR" ]]; then
     echo "Found Defaults Files.  Reading in Info"
     SUPPORT_DIR=$(defaults read "$DEFAULTS_DIR" SupportFiles)
     SD_BANNER_IMAGE="${SUPPORT_DIR}$(defaults read "$DEFAULTS_DIR" BannerImage)"
-    SPACING=$(defaults read "$DEFAULTS_DIR" BannerPadding)
+    BANNER_TEXT_PADDING=$(defaults read "$DEFAULTS_DIR" BannerPadding)
+    BANNER_SUBTITLE=$(defaults read "$DEFAULTS_DIR" BannerSubtitle)
+    BANNER_TEXT_COLOR=$(defaults read "$DEFAULTS_DIR" TitleFontColor)
 else
+    echo "Defaults Files Not Found.  Creating with default values"
     SUPPORT_DIR="/Library/Application Support/GiantEagle"
     SD_BANNER_IMAGE="${SUPPORT_DIR}/SupportFiles/GE_SD_BannerImage.png"
-    SPACING=5 #5 spaces to accommodate for icon offset
+    BANNER_TEXT_PADDING=10 #10 spaces to accommodate for icon offset
+    BANNER_SUBTITLE=""
+    BANNER_TEXT_COLOR="white"
 fi
-BANNER_TEXT_PADDING="${(j::)${(l:$SPACING:: :)}}"
 
 # Log files location
 
@@ -84,7 +89,7 @@ LOG_FILE="${SUPPORT_DIR}/logs/${SCRIPT_NAME}.log"
 
 # Display items (banner / icon)
 
-SD_WINDOW_TITLE="${BANNER_TEXT_PADDING}EntraID Registration"
+SD_WINDOW_TITLE="EntraID Registration"
 OVERLAY_ICON="/Applications/Company Portal.app"
 SD_WPJ_IMAGE="${SUPPORT_DIR}/SupportFiles/WPJKeychain.png"
 
@@ -333,8 +338,9 @@ function welcomemsg ()
     --imagecaption "${messageimagecpation}"
 		--bannerimage "${SD_BANNER_IMAGE}"
 		--bannertitle "${SD_WINDOW_TITLE}"
+    --bannersubtitle "${BANNER_SUBTITLE}"
     --infobox "${SD_INFO_BOX_MSG}"
-    --titlefont shadow=1
+    --titlefont shadow=1,color=${BANNER_TEXT_COLOR},offset=${BANNER_TEXT_PADDING}
     --alignment center
     --moveable
 		--button1text "OK"
