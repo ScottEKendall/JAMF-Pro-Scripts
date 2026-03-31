f5901ee95f063abfd210b09f7effe8074e95d9b4
Scott Kendall <106453475+ScottEKendall@users.noreply.github.com>
2026-03-31 10:27:54 -0400

    Updated to support Dialog 3.10 features - includes banner subtitles, color & offset

--- BatteryInfo/BatteryInfo.sh
+++ BatteryInfo/BatteryInfo.sh
@@ -5,7 +5,7 @@
 # Written by: Scott E. Kendall
 #
 # Created: 01/25/2025
-# Last updated: 03/13/2026
+# Last updated: 04/01/2026
 #
 # Script Purpose: Prompt user if battery needs service
 #
@@ -22,13 +22,15 @@
 #       Fixed typos
 # 1.7 - Had to increase window height for Tahoe & SD v3.0
 # 1.8 - Changed JAMF 'policy -trigger' to JAMF 'policy -event'
+# 2.0 - Updated SD Vesion requirements to 3.1.0
+#       Addeed ability to set subtitle, color, and padding from defaults file
 
 ######################################################################################################
 #
 # Global "Common" variables
 #
 ######################################################################################################
-
+#set -x
 SCRIPT_NAME="BatteryInfo"
 LOGGED_IN_USER=$( scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
 USER_DIR=$( dscl . -read /Users/${LOGGED_IN_USER} NFSHomeDirectory | awk '{ print $2 }' )
@@ -44,7 +46,7 @@ ICON_FILES="/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/"
 # Swift Dialog version requirements
 
 SW_DIALOG="/usr/local/bin/dialog"
-MIN_SD_REQUIRED_VERSION="2.5.0"
+MIN_SD_REQUIRED_VERSION="3.1.0"
 [[ -e "${SW_DIALOG}" ]] && SD_VERSION=$( ${SW_DIALOG} --version) || SD_VERSION="0.0.0"
 
 SD_DIALOG_GREETING=$((){print Good ${argv[2+($1>11)+($1>18)]}} ${(%):-%D{%H}} morning afternoon evening)
@@ -71,13 +73,17 @@ if [[ -f "$DEFAULTS_DIR" ]]; then
     echo "Found Defaults Files.  Reading in Info"
     SUPPORT_DIR=$(defaults read "$DEFAULTS_DIR" SupportFiles)
     SD_BANNER_IMAGE="${SUPPORT_DIR}$(defaults read "$DEFAULTS_DIR" BannerImage)"
-    SPACING=$(defaults read "$DEFAULTS_DIR" BannerPadding)
+    BANNER_TEXT_PADDING=$(defaults read "$DEFAULTS_DIR" BannerPadding)
+    BANNER_SUBTITLE=$(defaults read "$DEFAULTS_DIR" BannerSubtitle)
+    BANNER_TEXT_COLOR=$(defaults read "$DEFAULTS_DIR" TitleFontColor)
 else
+    echo "Defaults Files Not Found.  Creating with default values"
     SUPPORT_DIR="/Library/Application Support/GiantEagle"
     SD_BANNER_IMAGE="${SUPPORT_DIR}/SupportFiles/GE_SD_BannerImage.png"
-    SPACING=5 #5 spaces to accommodate for icon offset
+    BANNER_TEXT_PADDING=10 #10 spaces to accommodate for icon offset
+    BANNER_SUBTITLE=""
+    BANNER_TEXT_COLOR="white"
 fi
-BANNER_TEXT_PADDING="${(j::)${(l:$SPACING:: :)}}"
 
 # Log files location
 
@@ -85,7 +91,7 @@ LOG_FILE="${SUPPORT_DIR}/logs/${SCRIPT_NAME}.log"
 
 # Display items (banner / icon)
 
-SD_WINDOW_TITLE+="Battery Condition"
+SD_WINDOW_TITLE="Battery Condition"
 SD_ICON="SF=minus.plus.batteryblock, color=green, weight=normal"
 
 HELPDESK_URL="https://gianteagle.service-now.com/ge?id=sc_cat_item&sys_id=227586311b9790503b637518dc4bcb3d"
@@ -113,6 +119,7 @@ function create_log_directory ()
     #
     # RETURN: None
 
+    LOG_DIR=${LOG_FILE%/*}
 	# If the log directory doesnt exist - create it and set the permissions
 	[[ ! -d "${LOG_DIR}" ]] && /bin/mkdir -p "${LOG_DIR}"
 	/bin/chmod 755 "${LOG_DIR}"
@@ -223,14 +230,15 @@ function welcomemsg ()
 	MainDialogBody=(
         --message "${messagebody}"
         --icon computer
+		--bannerimage "${SD_BANNER_IMAGE}"
+		--bannertitle "${SD_WINDOW_TITLE}"
+        --subtitle "${BANNER_SUBTITLE}"
+        --titlefont "shadow=1, offset=${BANNER_TEXT_PADDING}, color=${BANNER_TEXT_COLOR:l}"
         --overlayicon "${OVERLAY_ICON}"
 		--height 480
         --width 760
 		--ontop
-		--bannerimage "${SD_BANNER_IMAGE}"
-		--bannertitle "${SD_WINDOW_TITLE}"
         --infobox "${SD_INFO_BOX_MSG}"
-        --titlefont shadow=1
         --moveable
 		--button1text 'OK'
 		--buttonstyle center
