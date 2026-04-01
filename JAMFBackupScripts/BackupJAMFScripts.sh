ee625c9aeb9f19b069c28afef86234d1461e8dc7
Scott Kendall <106453475+ScottEKendall@users.noreply.github.com>
2026-04-01 10:41:37 -0400

    Updated to support Dialog 3.10 features - includes banner subtitles, color & offset

--- JAMFBackupScripts/BackupJAMFScripts.sh
+++ JAMFBackupScripts/BackupJAMFScripts.sh
@@ -5,7 +5,7 @@
 # by: Scott Kendall
 #
 # Written: 05/28/2025
-# Last updated: 03/16/2026
+# Last updated: 04/01/2026
 #
 # Script Purpose: Exract all of the scripts from the JAMF server and store them on a local drive
 #
@@ -20,12 +20,15 @@
 #       Fixed variable names in the defaults file section
 #       Fixed function to check which SS/SS+ is being used (again)
 #       Had to increase window height for Tahoe & SD v3.0
+# 2.0 - Updated SD Version requirements to 3.1.0
+#       Added ability to set subtitle, color, and padding from defaults file
 
 ######################################################################################################
 #
-# Gobal "Common" variables
+# Global "Common" variables
 #
 ######################################################################################################
+#set -x
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin
 LOGGED_IN_USER=$( scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
 USER_ID=$(id -u "$LOGGED_IN_USER")
@@ -42,7 +45,7 @@ ICON_FILES="/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/"
 # Swift Dialog version requirements
 
 SW_DIALOG="/usr/local/bin/dialog"
-MIN_SD_REQUIRED_VERSION="2.5.0"
+MIN_SD_REQUIRED_VERSION="3.1.0"
 [[ -e "${SW_DIALOG}" ]] && SD_VERSION=$( ${SW_DIALOG} --version) || SD_VERSION="0.0.0"
 
 # Make some temp files for this app
@@ -66,13 +69,17 @@ if [[ -f "$DEFAULTS_DIR" ]]; then
     echo "Found Defaults Files.  Reading in Info"
     SUPPORT_DIR=$(defaults read "$DEFAULTS_DIR" SupportFiles)
     SD_BANNER_IMAGE="${SUPPORT_DIR}$(defaults read "$DEFAULTS_DIR" BannerImage)"
-    SPACING=$(defaults read "$DEFAULTS_DIR" BannerPadding)
+    BANNER_TEXT_PADDING=$(defaults read "$DEFAULTS_DIR" BannerPadding)
+    BANNER_SUBTITLE=$(defaults read "$DEFAULTS_DIR" BannerSubtitle)
+    BANNER_TEXT_COLOR=$(defaults read "$DEFAULTS_DIR" TitleFontColor)
 else
+    echo "Defaults Files Not Found.  Creating with default values"
     SUPPORT_DIR="/Library/Application Support/GiantEagle"
     SD_BANNER_IMAGE="${SUPPORT_DIR}/SupportFiles/GE_SD_BannerImage.png"
-    SPACING=5 #5 spaces to accommodate for icon offset
+    BANNER_TEXT_PADDING=10 #10 spaces to accommodate for icon offset
+    BANNER_SUBTITLE=""
+    BANNER_TEXT_COLOR="white"
 fi
-BANNER_TEXT_PADDING="${(j::)${(l:$SPACING:: :)}}"
 
 # Support / Log files location
 
@@ -81,7 +88,7 @@ LOG_FILE="${SUPPORT_DIR}/logs/JAMFScriptBackup.log"
 
 # Display items (banner / icon)
 
-SD_WINDOW_TITLE="${BANNER_TEXT_PADDING}JAMF Script Backup"
+SD_WINDOW_TITLE="JAMF Script Backup"
 OVERLAY_ICON="/Applications/Self Service.app"
 SD_ICON_FILE="https://images.crunchbase.com/image/upload/c_pad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/vhthjpy7kqryjxorozdk"
 
@@ -207,17 +214,18 @@ function welcomemsg ()
 
 	MainDialogBody=(
         --message "$SD_DIALOG_GREETING $SD_FIRST_NAME. $message"
-        --titlefont shadow=1
-        --ontop
+        --titlefont "shadow=1, color=${BANNER_TEXT_COLOR}, offset=${BANNER_TEXT_PADDING}"
         --icon "${SD_ICON_FILE}"
         --overlayicon "${OVERLAY_ICON}"
         --bannerimage "${SD_BANNER_IMAGE}"
         --bannertitle "${SD_WINDOW_TITLE}"
+        --subtitle "${BANNER_SUBTITLE}"
         --infobox "${SD_INFO_BOX_MSG}"
         --helpmessage ""
         --width 800
         --height 480
         --ignorednd
+        --ontop
         --quitkey 0
         --button1text "OK"
         --button2text "Cancel"
